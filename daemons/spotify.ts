d3ca9d8b8ac76271f8cbf6826451a5508892da86
Gabriel Guerra <gfranklings@gmail.com>
2025-10-12 11:43:25 -0400

    fix: spotify daemon timeout

--- daemons/spotify.ts
+++ daemons/spotify.ts
@@ -3,10 +3,12 @@ import { timeout } from "ags/time";
 import Mpris from "gi://AstalMpris";
 import Notifd from "gi://AstalNotifd"
 
+import { S_PER_MS } from "../constants";
+
 const spotify = Mpris.Player.new("spotify");
 
 spotify.connect("notify::metadata", () => {
-  timeout(0.5, () => {
+  timeout(0.5 * S_PER_MS, () => {
     if (!spotify.get_available()) return;
     const notification = Notifd.Notification.new();
     notification.set_summary(spotify.get_title());
